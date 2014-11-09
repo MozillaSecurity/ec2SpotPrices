d613cdd790b7253396b1c31634d57524bbc004b2
Gary Kwong <gary@rumblingedge.com>
2014-11-09 09:58:15 +0800

    The number of hypothetical instances to spawn should use -n instead of -s.

--- get_ec2_spot_prices.py
+++ get_ec2_spot_prices.py
@@ -72,7 +72,7 @@ def parseArgs():
     parser.add_argument('-a', '--awsKeyId', required=True, help='Sets the AWS key ID. (Required)')
     parser.add_argument('-b', '--awsSecret', required=True,
                         help='Sets the AWS secret access key. (Required)')
-    parser.add_argument('-s', '--spawnNum', default=100, type=int,
+    parser.add_argument('-n', '--spawnNum', default=100, type=int,
                         help='Sets the hypothetical number of instances to be spawned.')
     parser.add_argument('-o', '--os', default='linux', choices=osChoices,
                         help='Sets the operating system. Choose from [' + '|'.join(osChoices) +
